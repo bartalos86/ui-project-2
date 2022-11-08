bdd5ae3e006f7632bda4ab3f9ca26d4f68450da1
Mark Bartalos <bartalos86@gmail.com>
2022-11-08 21:28:50 +0100

    Added tournament selection and better fitness function

--- AICollection.cs
+++ AICollection.cs
@@ -17,18 +17,40 @@ namespace genetic_algorithm
 
         private double maxPossibleFitness;
         private int customSize = 64;
-        private Func<double> goldWorth = () => 10 ;
+        private Func<double> goldWorth = () => 25;
 
-        public AICollection(double chanceOfMutation = 0.01)
+        private Action selectionFunction;
+        private Func<VirtualMachine, VirtualMachine, VirtualMachine> crossoverFunction;
+
+        public AICollection(double chanceOfMutation = 0.01, SelectionStyle selectionStyle = SelectionStyle.ROULETTE,
+            CrossoverStyle crossoverStyle = CrossoverStyle.GENE_CROSSOVER)
         {
             VirtualMachines = new List<VirtualMachine>();
             ChanceOfMutation = chanceOfMutation;
             Generation = 0;
+
+            if(selectionStyle == SelectionStyle.ROULETTE)
+            {
+                selectionFunction = DoRuletteEvolution;
+            }
+            else
+            {
+                selectionFunction = DoTournamentEvolution;
+            }
+
+            if(crossoverStyle == CrossoverStyle.GENE_CROSSOVER)
+            {
+                crossoverFunction = GeneCrossover;
+            }
+            else
+            {
+                crossoverFunction = Crossover;
+            }
         }
 
         public void InitializeAIs(int count = 20)
         {
-            MaxAIs = count > 20 ? count /2 : count;
+            MaxAIs = count; //> 20 ? count /2 : count;
             for (int i = 0; i < count; i++)
             {
                 VirtualMachines.Add(new VirtualMachine(ControlBounds, customSize));
@@ -56,12 +78,85 @@ namespace genetic_algorithm
                 ai.DoGeneration(FitnessFunction);
             }
 
-            DoEvolution();
+            selectionFunction.Invoke();
+        }
 
+        private void DoTournamentEvolution()
+        {
+            var aisInTournament = new List<VirtualMachine>();
+            var random = new Random();
+
+           
+
+            if (!SolutionFound)
+            {
+                double totalFitness = 0;
+                foreach (var vm in VirtualMachines)
+                    totalFitness += vm.Fitness;
+
+                Console.Clear();
+                if(VirtualMachines[0].Steps  != null)
+                Map.PrintOutPath(VirtualMachines[0].Steps);
+                Console.WriteLine($"Average fitness: {Math.Round((totalFitness / VirtualMachines.Count), 1)}");
+                Console.WriteLine($"Best fitness: {Math.Round(VirtualMachines[0].Fitness, 1)}");
+                Console.WriteLine($"Current generation: {Generation}");
+                System.Console.WriteLine($"Number of AIs: {VirtualMachines.Count}");
+            }
+
+   
+
+            var requiredAiCount = Math.Max(MaxAIs / 3, Math.Sqrt(MaxAIs)+3);
+            Console.WriteLine($"Required ai cound: {requiredAiCount}");
+
+            for(int i = 0; aisInTournament.Count < requiredAiCount; i++)
+            {
+                var ai = VirtualMachines[random.Next(VirtualMachines.Count)];
+                if (!aisInTournament.Contains(ai))
+                    aisInTournament.Add(ai);
+            }
+
+            aisInTournament.Sort();
+            aisInTournament.Reverse();
+
+            var maxForNewChildren = Math.Ceiling(Math.Sqrt(MaxAIs)) + 5;
+
+            var newGeneration = new List<VirtualMachine>();
+
+            //Elits for only mutation
+            for (int i = 0, counter = 0; i < 3; i++)
+            {
+                if (!newGeneration.Contains(VirtualMachines[counter]))
+                {
+                    var vm = VirtualMachines[counter++];
+                    vm.Memory = Mutate(vm.Memory);
+                    newGeneration.Add(vm);
+
+                }
+                else
+                    i--;
+
+            }
+
+            for (int i = 0; i< maxForNewChildren; i++)
+            {
+                for (int j = i+1; j < maxForNewChildren; j++)
+                {
+                    var newChild = crossoverFunction.Invoke(aisInTournament[i], aisInTournament[j]);
+                    newGeneration.Add(newChild);
+
+                    if (newGeneration.Count >= MaxAIs)
+                        break;
+                }
+
+                if (newGeneration.Count >= MaxAIs)
+                    break;
+            }
+
+            VirtualMachines = newGeneration;
 
         }
 
-        private void DoEvolution()
+        private void DoRuletteEvolution()
         {
             VirtualMachines.Sort();
             VirtualMachines.Reverse();
@@ -76,9 +171,10 @@ namespace genetic_algorithm
             {
                 Console.Clear();
                 Map.PrintOutPath(VirtualMachines[0].Steps);
-                Console.WriteLine($"Average fitness: {Math.Round((totalFitness / VirtualMachines.Count) / maxPossibleFitness * 100, 1)}%");
+                Console.WriteLine($"Average fitness: {Math.Round((totalFitness / VirtualMachines.Count), 1)}%");
                 Console.WriteLine($"Best fitness: {Math.Round(VirtualMachines[0].Fitness, 1)}");
                 Console.WriteLine($"Current generation: {Generation}");
+                System.Console.WriteLine($"Number of AIs: {VirtualMachines.Count}");
             }
           
 
@@ -89,9 +185,21 @@ namespace genetic_algorithm
             }
 
             var random = new Random();
-
             var successfulAIs = new List<VirtualMachine>();
-            for(int i = 0; i< VirtualMachines.Count; i++)
+
+             //Elits for crossover
+            for(int i  = 0, counter = 0; i < Math.Max(Math.Sqrt(MaxAIs)/2,5); i++)
+            {
+                if (!successfulAIs.Contains(VirtualMachines[counter]))
+                    successfulAIs.Add(VirtualMachines[counter++]);
+                    else
+                    i--;
+                    
+            }
+
+            //Lottery
+            int elitCount = successfulAIs.Count;
+            for(int i = 0; i< MaxAIs - elitCount; i++)
             {
                 var number = random.NextDouble() *100;
                 int counter = 0;
@@ -100,8 +208,8 @@ namespace genetic_algorithm
 
                 while (total < number)
                 {
-                    if (counter >= chanceDictionary.Count)
-                        break;
+                    // if (counter >= chanceDictionary.Count)
+                    //     break;
 
                     element = chanceDictionary[counter++];
                     total += element.Item1;
@@ -113,48 +221,71 @@ namespace genetic_algorithm
                
             }
 
-            //Elits
-            for(int i  = 0; i< VirtualMachines.Count /4; i++)
-            {
-                if (!successfulAIs.Contains(VirtualMachines[i]))
-                    successfulAIs.Add(VirtualMachines[i]);
-            }
+           var newGeneration = new List<VirtualMachine>();
 
-            if(successfulAIs.Count <= 0)
+            //Elits for only mutation
+            for (int i = 0, counter = 0; i < Math.Max(Math.Sqrt(MaxAIs) / 2, 5); i++)
             {
-                for (int i = 0; i < VirtualMachines.Count; i++)
+                if (!newGeneration.Contains(VirtualMachines[counter]))
                 {
-                        successfulAIs.Add(VirtualMachines[i]);
-                    Console.WriteLine("asd");
+                    var vm = VirtualMachines[counter++];
+                    vm.Memory = Mutate(vm.Memory);
+                    newGeneration.Add(vm);
+
                 }
+                else
+                    i--;
+
             }
 
-            int maxCap = successfulAIs.Count <= MaxAIs ? successfulAIs.Count : MaxAIs;
+
             successfulAIs.Sort();
             successfulAIs.Reverse();
-            //successfulAIs.Reverse();
 
-            //foreach (var ai in  successfulAIs)
-            //    Console.WriteLine($"Fitness: {ai.Fitness}");
+            //Fill out with normals
+            for (int i = 0; successfulAIs.Count < Math.Ceiling(Math.Sqrt(MaxAIs))+3; i++)
+            {
+                if (!successfulAIs.Contains(VirtualMachines[i]))
+                {
+                    var vm = VirtualMachines[i];
+                    successfulAIs.Add(vm);
 
-           var newGeneration = new List<VirtualMachine>();
-           for(int i = 0; i < maxCap; i++)
+                }
+
+            }
+            int maxCap = successfulAIs.Count <= MaxAIs ? successfulAIs.Count : MaxAIs;
+
+
+            Console.WriteLine($"Succesful count: {successfulAIs.Count}");
+
+
+            for (int i = 0; i < maxCap; i++)
             {
                 //Console.WriteLine("Fitness: " + successfulAIs[i].Fitness);
 
-                for (int j = i + 1;j < maxCap/2; j++)
+                for (int j = i + 1;j < maxCap; j++)
                 {
-                    var newChild = Crossover(successfulAIs[i], successfulAIs[j]);
+                    var newChild = crossoverFunction.Invoke(successfulAIs[i], successfulAIs[j]);
+                    if(newGeneration.Count >= MaxAIs){
+                        break;
+                    }
                     newGeneration.Add(newChild);
                 }
+
+                if(newGeneration.Count >= MaxAIs){
+                        break;
+                    }
             }
+
+            Console.WriteLine($"New generation count: {newGeneration.Count}");
+
 
             VirtualMachines = newGeneration;
                 
 
         }
 
-        private VirtualMachine ChanceCrossover(VirtualMachine parent1, VirtualMachine parent2)
+        private VirtualMachine GeneCrossover(VirtualMachine parent1, VirtualMachine parent2)
         {
             var child = new VirtualMachine(ControlBounds);
             var crossoverMemory = new List<Memory>();
@@ -186,7 +317,9 @@ namespace genetic_algorithm
         {
             var child = new VirtualMachine(ControlBounds);
             var crossoverMemory = new List<Memory>();
-            int crossoverPoint = parent1.Memory.Count/2;
+            var totalFitness = parent1.Fitness + parent2.Fitness;
+
+            int crossoverPoint = (int)Math.Floor(parent1.Memory.Count * (parent1.Fitness / totalFitness));
 
             for(int i = 0; i < parent1.Memory.Count; i++)
             {
@@ -236,38 +369,45 @@ namespace genetic_algorithm
 
         private double FitnessFunction(VirtualMachine machine) {
 
-            double score = 0;
+            double score = 0.01;
             if (machine.Steps == null)
-                return 0;
+                return 0.01;
 
             var pathData = Map.PerfromsSteps(machine.Steps);
             var position = pathData.Item1;
             var goldsCollected = pathData.Item2;
-            //score -= machine.Steps.Count * 0.05;
             bool wereNotCollectedGolds = false;
 
             //var rightSteps = machine.Steps.FindAll((step) => step == Step.P);
             //score -= rightSteps.Count * 0.1;
             //score -= machine.Steps.FindAll((step) => step == Step.H).Count * 0.1;
 
-            foreach (var gold in goldsCollected)
-            {
-                score += goldWorth.Invoke();
-            }
+            // foreach (var gold in goldsCollected)
+            //{
+            score = Math.Pow(goldWorth.Invoke(),goldsCollected.Count);
+            //score = goldsCollected.Count * goldWorth.Invoke();
+            var stepPenalty = Math.Pow(1.45, machine.Steps.Count / 2);
 
-            foreach(var goldPosition in Map.GoldPositions)
+            if(score - stepPenalty > 0.1)
+                score = score - stepPenalty;
+
+            // }
+
+            foreach (var goldPosition in Map.GoldPositions)
             {
                 if (goldsCollected.ContainsByHash(goldPosition))
                     continue;
 
-                score += 2 / (Map.GetDistance(goldPosition, position)+1);
+                score += Math.Pow(1.40,goldWorth.Invoke() / (Map.GetDistance(goldPosition, position)+1));
                 wereNotCollectedGolds = true;
             }
 
-            if(score >= maxPossibleFitness || !wereNotCollectedGolds)
+            if( goldsCollected.Count == Map.GoldPositions.Count)
             {
                 Console.Clear();
                 Console.WriteLine("Final AI found");
+                Console.WriteLine($"Best fitness: {Math.Round(machine.Fitness, 1)}");
+                System.Console.WriteLine($"Number of AIs: {VirtualMachines.Count}");
                 Console.WriteLine($"Total generations: {Generation}");
                 Map.PrintOutPath(machine.Steps);
                 SolutionFound = true;
--- Program.cs
+++ Program.cs
@@ -7,16 +7,27 @@ namespace genetic_algorithm
     {
         public static void Main(string[] args)
         {
-            AICollection swarm = new AICollection(0.05);
+            AICollection swarm = new AICollection(0.05, SelectionStyle.ROULETTE, CrossoverStyle.GENE_CROSSOVER);
             string file = args.Length > 0 ? args[0] : "testMap.txt";
 
             Map startingMap = ReadMap(file);
             startingMap.PrintMap();
             swarm.SetupMap(startingMap);
-            swarm.InitializeAIs(50);
+            swarm.InitializeAIs(100);
 
             while (!swarm.SolutionFound)
+            {
                 swarm.DoGeneration();
+            }
+
+            Console.WriteLine("do you want to continue? (c/n)");
+            if (swarm.SolutionFound && Console.ReadLine() == "c")
+            {
+                while (true)
+                {
+                    swarm.DoGeneration();
+                }
+            }
 
 
         }
